0d03dccf02e1219059b4f47865f7c565b2151e8d
Pinterid <david.pinterics@gmail.com>
2020-07-31 11:15:24 +0200

    Add basic routes
    The basic routes were added to the urls

--- tastic/urls.py
+++ tastic/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index_route"),
+    #TODO Views have to be changed if the views get updated
+    path('login/', views.index, name='login_route'),
+    path('logout/', views.index, name='logout_route'),
+    path("throughputs/", views.index, name="throughput_charts_route"),
+    path("burndowns/", views.index, name="burn_down_charts_route"),
+    path("features/", views.index, name="features_route"),
+    path("dods/", views.index, name="dods_route"),
+    path("stories/", views.index, name="user_stories_route"),
+    path("reports/", views.index, name="status_reports_route"),
 ]
