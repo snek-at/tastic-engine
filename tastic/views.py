fbb5af38f6ca7ded6a2db8f80e889f2c89717cca
Pinterid <david.pinterics@gmail.com>
2020-08-03 15:00:19 +0200

    Create Dummy Data
    Dummy data for features was added.

--- tastic/views.py
+++ tastic/views.py
@@ -4,3 +4,66 @@ from django.shortcuts import render
 def index(request):
     # Render site
     return render(request, "index.html")
+
+
+def features(request):
+    # Dummy Data
+    values = {
+        "sortedBy": "Newest",
+        "files": [
+            {
+                "name": "File1",
+                "createdAt": "29/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File2",
+                "createdAt": "30/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+            {
+                "name": "File3",
+                "createdAt": "31/07/2020",
+                "viewUrl": "https://snek.at",
+                "downloadUrl": "https://snek.at",
+            },
+        ],
+    }
