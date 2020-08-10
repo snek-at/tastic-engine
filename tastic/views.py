3e3720516a6456b1608ae483e7fb1edf9197cfae
Pinterid <david.pinterics@gmail.com>
2020-08-10 15:48:17 +0200

    Implement global variables
    The global variables now partly replace the dummy data.

--- tastic/views.py
+++ tastic/views.py
@@ -8,6 +8,9 @@ lineData = {}
 
 
 def getData():
+    global lineData
+    global barData
+
     client = githubClient()
 
     # Throughput
@@ -25,179 +28,17 @@ def getData():
 
 
 def index(request):
+    global lineData
+    global barData
+
+    # This function will be moved into a cronjob, when the program gets moved into a container
+    if lineData == {} or barData == {}:
+        getData()
+    # determined = client.determineCards(cards, columns)
     # Dummy data
     values = {
-        "lineData": {
-            "labels": ["25.07", "26.07", "27.07", "28.07", "29.07", "30.07", "31.07"],
-            "datasets": [
-                {
-                    "label": "Actual time remaining",
-                    "data": [6, 6, 5, 2, 1, 0, 0],
-                    "backgroundColor": [
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Ideal time remaining",
-                    "data": [6, 5, 4, 3, 2, 1, 0],
-                    "backgroundColor": [
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-            ],
-        },
-        "barData": {
-            "labels": ["25.07", "26.07", "27.07", "28.07", "29.07", "30.07", "31.07"],
-            "datasets": [
-                {
-                    "label": "Requirements",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Features",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Opportunities",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Enhancements",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Bugs",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-            ],
-        },
+        "lineData": lineData,
+        "barData": barData,
         "story": {
             "name": "User Story 1",
             "createdAt": "29/07/2020",
@@ -568,191 +409,18 @@ def reports(request):
 
 
 def throughputs(request):
+    global barData
     # Dummy data
-    values = {
-        "filteredBy": "All time",
-        "barData": {
-            "labels": ["25.07", "26.07", "27.07", "28.07", "29.07", "30.07", "31.07"],
-            "datasets": [
-                {
-                    "label": "Requirements",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                        "rgba(144, 249, 162, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                        "rgba(144, 249, 162, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Features",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                        "rgba(186, 5, 107, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                        "rgba(186, 5, 107, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Opportunities",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                        "rgba(89, 59, 165, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                        "rgba(89, 59, 165, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Enhancements",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                        "rgba(162, 238, 239, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                        "rgba(162, 238, 239, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Bugs",
-                    "data": [1, 1, 1, 1, 1, 1, 1],
-                    "backgroundColor": [
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                        "rgba(215, 58, 74, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                        "rgba(215, 58, 74, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-            ],
-        },
-    }
+    values = {"filteredBy": "All time", "barData": barData}
 
     # Render site
     return render(request, "pages/throughputs.html", values)
 
 
 def burndowns(request):
+    global lineData
     # Dummy data
-    values = {
-        "filteredBy": "All time",
-        "lineData": {
-            "labels": ["25.07", "26.07", "27.07", "28.07", "29.07", "30.07", "31.07"],
-            "datasets": [
-                {
-                    "label": "Actual time remaining",
-                    "data": [6, 6, 5, 2, 1, 0, 0],
-                    "backgroundColor": [
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                        "rgba(240, 52, 52, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                        "rgba(240, 52, 52, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-                {
-                    "label": "Ideal time remaining",
-                    "data": [6, 5, 4, 3, 2, 1, 0],
-                    "backgroundColor": [
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                        "rgba(0, 230, 64, 0.5)",
-                    ],
-                    "borderColor": [
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                        "rgba(0, 230, 64, 1)",
-                    ],
-                    "borderWidth": 2,
-                },
-            ],
-        },
-    }
+    values = {"filteredBy": "All time", "lineData": lineData}
 
     # Render site
     return render(request, "pages/burndowns.html", values)
