bf32abcae2488e8b74dbfa959bee84a9684fadbf
Pinterid <david.pinterics@gmail.com>
2020-08-10 15:43:56 +0200

    Add global variables
    Global chart variables were added.

--- tastic/views.py
+++ tastic/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
+barData = {}
+lineData = {}
 def index(request):
     # Dummy data
     values = {
