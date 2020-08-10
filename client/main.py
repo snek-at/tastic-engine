81b8219c7265358c60fe66a8542b1680ccebfff0
Pinterid <david.pinterics@gmail.com>
2020-08-10 15:29:45 +0200

    Create client constructor
    The constructor for the GitHub client was created.

---
+++ client/main.py
@@ -0,0 +1,14 @@
+# Class
+class githubClient:
+    def __init__(
+        self,
+        api_url="https://api.github.com",
+        labels=["Feature", "Opportunity", "Requirement", "bug", "enhancement"],
+        org="snek-at",
+        project_start=datetime(2020, 7, 15),
+    ):
+        # VARIABLES
+        self.api_url = api_url
+        self.labels = ["Feature", "Opportunity", "Requirement", "bug", "enhancement"]
+        self.org = org
+        self.project_start = project_start
