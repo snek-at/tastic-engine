5abdaf614b59e87a390487cd49e7924e99029ba3
Pinterid <david.pinterics@gmail.com>
2020-08-10 15:45:08 +0200

    Create client instance
    The client is now being used by the webserver.

--- tastic/views.py
+++ tastic/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from client.main import githubClient
 
 barData = {}
 lineData = {}
+
+
+def getData():
+    client = githubClient()
+
+
 def index(request):
     # Dummy data
     values = {
