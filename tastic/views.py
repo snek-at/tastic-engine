3c78e1294fce6728808cba26a00543dbe900018c
Pinterid <david.pinterics@gmail.com>
2020-08-10 17:48:47 +0200

    Import models
    The models are now imported in the views.

--- tastic/views.py
+++ tastic/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from client.main import githubClient
-
+from tastic.models import Throughput, BurnDown
 barData = {}
 lineData = {}
 
