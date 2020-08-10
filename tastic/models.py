fe56db4125bc76535f666d99228a2e096cad1603
Pinterid <david.pinterics@gmail.com>
2020-08-10 17:46:20 +0200

    Add burn down model
    The model for burn down data was added.

--- tastic/models.py
+++ tastic/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class BurnDown(models.Model):
+    date = models.DateTimeField(primary_key=True)
+    ideal = models.IntegerField()
+    actual = models.IntegerField()
