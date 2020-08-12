17a444ed6a8a1f6e8ff4538f44965840e7ba2301
Pinterid <david.pinterics@gmail.com>
2020-08-12 16:40:52 +0200

    Update imports
    The imports were updated to enable file download.

--- tastic/views.py
+++ tastic/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import FileResponse, Http404
 
 from client.main import githubClient
 from tastic.models import Throughput, BurnDown, Features, Dods, Stories
