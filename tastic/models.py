5437beab6d6c783328b3624dd06cde660d1ba4ea
Pinterid <david.pinterics@gmail.com>
2020-08-12 16:31:34 +0200

    Add feature model
    The model for features was added.

--- tastic/models.py
+++ tastic/models.py
@@ -14,3 +14,9 @@ class BurnDown(models.Model):
     date = models.DateTimeField(primary_key=True)
     ideal = models.IntegerField()
     actual = models.IntegerField()
+
+
+class Features(models.Model):
+    filename = models.CharField(max_length=20, primary_key=True)
+    date = models.DateTimeField(auto_now=True)
+    path = models.CharField(max_length=200, null=True)
