d530ad39bfeb438cfc062ca1720564ef4f6ddb6b
Pinterid <david.pinterics@gmail.com>
2020-08-13 14:32:57 +0200

    Improve code quality
    The code quality was improved to satisfy codacy.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
-from django.contrib.auth.models import User
 
 
 def user_login(request):
